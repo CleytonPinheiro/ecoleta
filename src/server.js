15b9de6975a4985a42a318eeb32d8ef18873e341
CleytonPinheiro <cleyton_pin@hotmail.com>
2020-06-04 20:33:44 -0300

    Trabalhando com nunjucks, deixando o HTML mais dinamico e inteligente

--- src/server.js
+++ src/server.js
@@ -8,13 +8,25 @@ const server = express()
 //Configurando a pasta publica
 server.use(express.static("public"))
 
+
+//UTILIZANDO TEMPLATE ENGINES
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 //Configurando caminhos da app. page home. Req: Requisição, res: Resposta
 server.get("/", (req,res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html", {title: "Um título"})
 })
 
 server.get("/create-point", (req,res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req,res) => {
+    return res.render("search-results.html")
 })
 
 //Subindo servidor
